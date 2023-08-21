0acf519deba58ac22218a4b51f040c03f8bfb84b
tahmid530 <tahmidahmed530@gmail.com>
2023-08-21 19:08:05 +0600

    modal added

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const btnCopy = document.getElementById('btn-copy');
+
+    btnCopy.addEventListener('click', function () {
+        const buttonText = btnCopy.textContent;
+
+        navigator.clipboard.writeText(buttonText)
+            .then(() => {
+                btnCopy.textContent = 'Copied!';
+                setTimeout(() => {
+                    btnCopy.textContent = buttonText;
+                }, 2000);
+            })
+            .catch(err => {
+                console.error('Unable to copy text: ', err);
+            });
+    });
+});
+
+
+
+
 let total = 0;
 
 function handleClick(target) {
-    const cardName = target.childNodes[5].innerText;
-    const li = document.createElement('li');
-    li.innerText = cardName;
     const selectedItemsField = document.getElementById('selected-items');
-    selectedItemsField.appendChild(li);
+    const cardName = target.childNodes[5].innerText;
+    const ol = document.createElement('ol');
+    ol.innerText = cardName;
+    selectedItemsField.appendChild(ol);
 
     const price = target.childNodes[7].innerText.split(' ')[0];
-    sum = parseFloat(total) + parseFloat(price) + ' TK';
+    total = parseFloat(total) + parseFloat(price) + ' TK'
 
-    const totalPriceField = document.getElementById('total-price');
-    totalPriceField.innerText = sum;
-
+    const totalPriceField = (document.getElementById('total-price').innerText = total);
 }
+
+const btnPurchase = document.getElementById('btn-purchase').addEventListener('click', () => {
+    my_modal_1.showModal()
+});
+const btnHome = document.getElementById('btn-home').addEventListener('click', () => {
+    window.location.reload()
+});
+
+//btnPurchase.addEventListener('click', () => {
+   /// my_modal_1.showModal()
+//});
+
+//btnPurchase.addEventListener('click',()=>{
+   // my_modal_1.showModal()
+//})
